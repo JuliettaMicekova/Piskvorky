724190f928279e095a144c8e44b293338bd57920
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-04-20 23:38:16 +0200

    finalni verze JS

--- game.js
+++ game.js
@@ -8,13 +8,15 @@ document.querySelectorAll('.board-field__btn').forEach((btn) => {
   btn.addEventListener('click', () => {
     if (draw === 'circle') {
       btn.classList.add('.board-field--circle');
+      btn.innerHTML = `<img src="circle.svg"/>`;
       btn.setAttribute('disabled', true);
-      drawPlayerElm.src = 'circle.svg';
+      drawPlayerElm.src = 'cross.svg';
       draw = 'cross';
     } else {
       btn.classList.add('.board-field--cross');
       btn.setAttribute('disabled', true);
-      drawPlayerElm.src = 'cross.svg';
+      btn.innerHTML = `<img src="cross.svg"/>`;
+      drawPlayerElm.src = 'circle.svg';
       draw = 'circle';
     }
   });
