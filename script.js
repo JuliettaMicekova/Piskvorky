1d39ce39bddcc85be52964de3558e11f560b4ca3
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-04-21 01:11:39 +0200

    finalni verze

--- script.js
+++ script.js
@@ -7,15 +7,13 @@ const drawPlayerElm = document.querySelector('.draw-player');
 document.querySelectorAll('.board-field__btn').forEach((btn) => {
   btn.addEventListener('click', () => {
     if (draw === 'circle') {
-      btn.classList.add('.board-field--circle');
-      btn.innerHTML = `<img src="circle.svg"/>`;
+      btn.classList.add('board__field--circle');
       btn.setAttribute('disabled', true);
       drawPlayerElm.src = 'cross.svg';
       draw = 'cross';
     } else {
-      btn.classList.add('.board-field--cross');
+      btn.classList.add('board__field--cross');
       btn.setAttribute('disabled', true);
-      btn.innerHTML = `<img src="cross.svg"/>`;
       drawPlayerElm.src = 'circle.svg';
       draw = 'circle';
     }
